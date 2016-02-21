334c6fa4143094fb12f3d5ac5a4dddafd8e359a0
James Lawrence <jamdlaw@yahoo.com>
2016-02-20 23:41:52 -0800

    clearned up code by indenting and deleting spaces

--- scripts.js
+++ scripts.js
@@ -1,42 +1,31 @@
 /* First script to use jquery to make a div draggable
    and another droppable
   */
-
- var score = 0;
- var previousLevel;
-
+var score = 0;
+var previousLevel;
 var levels = { 
-     
-	 "easy": 
-			[
-			 
-				{
-				"name":"above",
-				"place":["col2row1", "col3row1"],
-				"message":"Place Monkey above the rainbow"
-				},
-			 
-				{
-				"name":"beside", 
-				"place":["col1row2", "col3row2"],
-				"message":"Place Monkey beside the rainbow"
-				},
-			 
-				{
-				"name":"below",
-				"place":["col1row3", "col2row3", "col3row3"],
-				"message":"Place Monkey below the rainbow"
-				}
-			]
-	 };
- 
- 
-$(document).ready(function () {
+"easy":[
+		{
+		"name":"above",
+		"place":["col2row1", "col3row1"],
+		"message":"Place Monkey above the rainbow"
+		},
+		{
+		"name":"beside", 
+		"place":["col1row2", "col3row2"],
+		"message":"Place Monkey beside the rainbow"
+		},
+		{
+		"name":"below",
+		"place":["col1row3", "col2row3", "col3row3"],
+		"message":"Place Monkey below the rainbow"
+		}
+	]
+};
+$(document).ready(function(){
     "use strict";
-	
 	// set the monkey dive
     $("#col1row1").draggable({revert: "invalid"});
-	
 	/*
 	//let the user select difficulty  
 	$(function() {
@@ -53,7 +42,6 @@ $(document).ready(function () {
 		}
 	});
 	*/
-	
 	//dialog for message to the player
 	$("#message").dialog({
 		autoOpen: false,
@@ -66,39 +54,30 @@ $(document).ready(function () {
 			duration: 1000
 		}
 	});
-	
     //setupLevel(); 
-	
 	//setup modal form that will handel 
 	//level changes
 	$( "#dialog-form" ).dialog({
-	autoOpen: true,
-	height: 300,
-	width: 350,
-	modal: true,
-	text: "you did it",
-	buttons: {
-		New: function (){
-			var difficulty = $('#level_select option:selected').val();
-			$(this).dialog('close');
-			setupLevel(difficulty, 'new');
-			},
-		Replay: function () {
-			$(this).dialog('close');
-			setupLevel(difficulty, 'replay');
+		autoOpen: true,
+		height: 300,
+		width: 350,
+		modal: true,
+		text: "you did it",
+		buttons: {
+			New: function(){
+				var difficulty = $('#level_select option:selected').val();
+				$(this).dialog('close');
+				setupLevel(difficulty, 'new');
+				},
+			Replay: function(){
+				$(this).dialog('close');
+				setupLevel(difficulty, 'replay');
+				}
 			}
-	
-		}
-	
 	});
-	
-
 });
-
-
 function makeDroppable(droppables){
 	// make each div droppable
-
 	$.each( droppables, function(i ,droppable) {
 	//console.log(droppable);		
 			$('#' + droppable).droppable({
@@ -113,15 +92,11 @@ function makeDroppable(droppables){
 				}		
 			});
 		});
-
-	
 } // end of makeDroppable
-
 //function to create the new droppable divs 
-function setupLevel(difficulty, mode = 'new') {
-		
+function setupLevel(difficulty, mode = 'new'){
 	//clear droppable
-	if(typeof level !== 'undefined') {
+	if(typeof level !== 'undefined'){
 		clearDroppable(level.place);
 	}
 	// get the levels for the difficulity
@@ -131,62 +106,46 @@ function setupLevel(difficulty, mode = 'new') {
 	else{
 		level = previousLevel;
 	}
-	
 	//display message
 	displayMessage(level.message);
-	
 	//set droppable divs
 	makeDroppable(level.place);	
-		
 	}
-
-function clearDroppable(droppables)
-	{
+function clearDroppable(droppables){
 	$.each( droppables, function(i ,droppable) {
 			$('#' + droppable).droppable('destroy');
 		});
-
-	}
-	
-function getRandomLevel(difficulty) {
+}
+function getRandomLevel(difficulty){
 	var selectedLevel = levels[difficulty];
 	var randomLavel = selectedLevel[Math.floor(Math.random()*selectedLevel.length)];
 	if (randomLavel == previousLevel) {
 	getRandomLevel();
 	}
-	
-	return randomLavel;
-	
+	return randomLavel;	
 }
-
-function displayMessage(message) {
+function displayMessage(message){
 	$('#message').empty();
 	$('#message').append('<p>' +  message + '</p>');
 	$('#message').dialog('open');
-	}
-
-
-	
-
+}
 //call this function to return monkey after 
 // level is complete.
-var revert = function() {
-		$("#col1row1").draggable({revert: "valid"});
-		var r = $.Deferred();
-		setTimeout(function () {
-	    // and call `resolve` on the deferred object, once you're done
-		r.resolve();
-		}, 500);
-
-		// return the deferred object
-		return r;		
+var revert = function(){
+	$("#col1row1").draggable({revert: "valid"});
+	var r = $.Deferred();
+	setTimeout(function (){
+    // and call `resolve` on the deferred object, once you're done
+	r.resolve();
+	}, 500);
+	// return the deferred object
+	return r;		
 };
-
 // call this function when monkey is returned 
 //to beginning postion	
-var reset = function() {
-		$("#col1row1").draggable({revert: "invalid"});
-		//makeDroppable();
-		//setupLevel();
+var reset = function(){
+	$("#col1row1").draggable({revert: "invalid"});
+	//makeDroppable();
+	//setupLevel();
 };
 
